7b08a493c3f3ac90aa6f526395339f0fbeb0cc18
jskartes <joshuakartes@gmail.com>
2025-07-06 14:58:12 -0600

    Install and implement Sentry error handling

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,5 +1,7 @@
 import CustomButton from "@/components/CustomButton";
 import CustomInput from "@/components/CustomInput";
+import { signIn } from "@/lib/appwrite";
+import * as Sentry from "@sentry/react-native";
 import { Link, router } from "expo-router";
 import { useState } from "react";
 import { Alert, Text, View } from "react-native";
@@ -9,16 +11,17 @@ const SignIn = () => {
   const [form, setForm] = useState({ email: "", password: "" });
 
   const submit = async () => {
-    if (!form.email || !form.password) {
+    const { email, password } = form;
+    if (!email || !password) {
       return Alert.alert("Error", "Please enter a valid e-mail and password.");
     }
     setIsSubmitting(true);
     try {
-      // Call Appwrite signIn function
-      Alert.alert("Success!", "User signed in successfully.");
+      await signIn({ email, password });
       router.replace("/");
     } catch (error: any) {
       Alert.alert("Error", error.message);
+      Sentry.captureEvent(error);
     } finally {
       setIsSubmitting(false);
     }
