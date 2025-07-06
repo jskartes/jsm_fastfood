e5ccdd54a0f4419441779e36dcbe9875cf6ee2f1
jskartes <joshuakartes@gmail.com>
2025-07-06 11:50:54 -0600

    Configure custom Nativewind styles and fonts

--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+      <Text className="text-5xl text-center font-quicksand-bold text-primary">
+        Welcome to my React Native app!
       </Text>
     </View>
   );
